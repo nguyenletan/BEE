6446b8891cab73c5db0f97971c593a5cb7392b91
nguyenletan <nguyenletan@gmail.com>
2020-12-23 00:55:54 +1100

    fixed router error on netlify host.

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App () {
   return (
     <div className="App container">
       <Switch>
-        <Route path="/Login" component={Login} exact />
+        <Route path="/Login" component={Login} />
         <Redirect to="/Login" />
       </Switch>
     </div>
