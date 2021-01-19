b2792d67001a30d2e333a0a7d77ad67dc7947620
nguyenletan <nguyenletan@gmail.com>
2021-01-19 17:18:14 +1100

    added icons to table

--- src/pages/building/comparision/components/ImprovementMeasures.js
+++ src/pages/building/comparision/components/ImprovementMeasures.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import openingsImg from '../../../../assets/images/opening.png'
+import coolingImg from '../../../../assets/images/cooling.svg'
+import lightingImg from '../../../../assets/images/lighting.svg'
 
 const ImprovementMeasuresWrapper = styled.div`
   padding: 20px;
@@ -47,6 +50,24 @@ const FirstTd = styled.td`
   text-align: left !important;
 `
 
+const Image = styled.img`
+
+`
+
+const ImageWrapper = styled.span`
+  width: 45px;
+  text-align: center;
+  margin-right: 5px;
+  display: inline-block;
+`
+
+const InfoButton = styled.button`
+  border-radius: 15px;
+  padding-left: 18px;
+  padding-right: 18px;
+  text-transform: uppercase;
+`
+
 const improvementMeasuresData = [
   {
     measures: 'LED Replacement',
@@ -75,17 +96,39 @@ const improvementMeasuresData = [
 ]
 
 const ImprovementMeasures = () => {
-  const rows = improvementMeasuresData.map(item => (
-    <tr>
-      <FirstTd>{item.measures}</FirstTd>
-      <td>{item.investmentCost}</td>
-      <td>{item.energySavings}</td>
-      <td>{item.energyCostSavings}</td>
-      <td>{item.paybackPeriod}</td>
-      <td>{item.co2EmissionsAvoided}</td>
-      <td><button className="btn btn-primary">Info</button> </td>
-    </tr>
-  ))
+  const rows = improvementMeasuresData.map(item => {
+    let imgSrc = lightingImg
+    let width = 60
+    switch (item.measures) {
+      case 'chiller unit replacement':
+        imgSrc = coolingImg
+        width = 30
+        break
+      case 'double glaze lowe windows':
+        imgSrc = openingsImg
+        width = 45
+        break
+      default:
+        imgSrc = lightingImg
+        width = 25
+        break
+
+    }
+    return (
+      <tr>
+        <FirstTd><ImageWrapper><Image src={imgSrc} alt={item.measures} width={width}/></ImageWrapper>{item.measures}
+        </FirstTd>
+        <td>{item.investmentCost}</td>
+        <td>{item.energySavings}</td>
+        <td>{item.energyCostSavings}</td>
+        <td>{item.paybackPeriod}</td>
+        <td>{item.co2EmissionsAvoided}</td>
+        <td>
+          <InfoButton className="btn btn-primary btn-sm">Info</InfoButton>
+        </td>
+      </tr>
+    )
+  })
 
   return (
     <ImprovementMeasuresWrapper>
