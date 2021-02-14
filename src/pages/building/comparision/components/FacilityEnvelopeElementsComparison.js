d1307332b933ff8a5697060b64af45f673284301
nguyenletan <nguyenletan@gmail.com>
2021-02-14 12:31:56 +1100

    added comparison popup

--- src/pages/building/comparision/components/FacilityEnvelopeElementsComparison.js
+++ src/pages/building/comparision/components/FacilityEnvelopeElementsComparison.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Modal from 'react-bootstrap/modal'
 import roofImg from '../../../../assets/images/roof.png'
 import wallImg from '../../../../assets/images/wall.png'
 import openingsImg from '../../../../assets/images/opening.png'
 import floorImg from '../../../../assets/images/floor.png'
 import { getCurrentColor } from '../../../../Utilities'
 import EnergySquare from './EnergySquare'
+import { Container } from 'react-bootstrap'
 
 const FacilityEnvelopeElementsComparisonTitle = styled.h3`
   font-size: 1.15rem;
@@ -51,6 +53,7 @@ const ItemRow = styled.div`
   margin-bottom: 10px;
   padding: 5px;
   transition: box-shadow 100ms ease-in-out;
+
   &:hover {
     box-shadow: 0px 0px 5px 1px #ddd;
   }
@@ -61,6 +64,45 @@ const HeadRow = styled.div`
   padding: 5px;
 `
 
+const PopupIcon = styled.img`
+  margin-right: .5rem;
+`
+
+const PopupTitle = styled.h3`
+  font-size: 1.1rem;
+  font-weight: 700;
+  line-height: 54px;
+  margin-bottom: 0;
+  text-align: center;
+  text-transform: capitalize;
+`
+
+const ComparisonTable = styled.div`
+  font-size: 0.9rem;
+  margin-bottom: 1.5rem;
+  margin-top: 1.5rem;
+`
+
+const PopupDescription = styled.p`
+  font-size: 0.85rem;
+`
+
+const FirstLine = styled.span`
+  display: block;
+  font-size: 0.9rem;
+`
+
+const SecondLine = styled.span`
+  display: block;
+  font-size: .85rem;
+  color: var(--primary);
+`
+
+const CloseBtn = styled.button`
+  float: right;
+  border-width: 0;
+`
+
 const FacilityEnvelopeElementsComparison = () => {
 
   const facilityEnvelopeComparisonData = {
@@ -68,6 +110,113 @@ const FacilityEnvelopeElementsComparison = () => {
     wall: { current: 'D', potentialBestInClass: 'B' },
     openings: { current: 'C', potentialBestInClass: 'B' },
     floor: { current: 'E', potentialBestInClass: 'C' },
+  }
+
+  const [show, setShow] = useState(false)
+  const [popUpProps, setPopupProps] = useState({})
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const openPopup = (data) => {
+    console.log('popup')
+    setPopupProps(data)
+    handleShow()
+  }
+
+  const Popup = (props) => {
+    const { type, current, potentialBestInClass } = props?.data
+
+    let icon = ''
+    switch (type) {
+      case 'roof':
+        icon = roofImg
+        break
+      case 'wall':
+        icon = wallImg
+        break
+      case 'openings':
+        icon = openingsImg
+        break
+      case 'floor':
+        icon = floorImg
+        break
+      default:
+        break
+    }
+
+    console.log(icon)
+
+    return (
+      <Modal show={show} onHide={handleClose} size="sm">
+        <Modal.Body>
+          <Container className="mt-4">
+          <div className="d-flex justify-content-center">
+            <PopupIcon src={icon} alt="roof" title="roof"/>
+            <PopupTitle>{type} Performance</PopupTitle>
+          </div>
+          <ComparisonTable>
+            <div className="row mt-3">
+              <div className="col-6 text-center">
+                Current Energy Performance
+              </div>
+              <div className="col-6 text-center">
+                Potential<br/>Best-In-Class
+              </div>
+            </div>
+
+            <div className="row mt-3">
+              <div className="col-6 text-center">
+                <EnergySquare color={getCurrentColor(current)}
+                              text={current}/>
+              </div>
+              <div className="col-6 text-center">
+                <EnergySquare color={getCurrentColor(potentialBestInClass)}
+                              text={potentialBestInClass}/>
+              </div>
+            </div>
+
+            <div className="row mt-3">
+              <div className="col-6 text-center flex-column">
+                <FirstLine>Windows Glazing Type</FirstLine>
+                <SecondLine>Single Glazed</SecondLine>
+              </div>
+              <div className="col-6 text-center">
+                <FirstLine>Windows Glazing Type</FirstLine>
+                <SecondLine>Double Glazed</SecondLine>
+              </div>
+            </div>
+
+            <div className="row mt-3">
+              <div className="col-6 text-center">
+                <FirstLine>U-Value W/m2K</FirstLine>
+                <SecondLine>1.53</SecondLine>
+              </div>
+              <div className="col-6 text-center">
+                <FirstLine>U-Value W/m2K</FirstLine>
+                <SecondLine>0.87</SecondLine>
+              </div>
+            </div>
+
+            <div className="row mt-3">
+              <div className="col-6 text-center">
+                <FirstLine>Shading Coefficient</FirstLine>
+                <SecondLine>0.9</SecondLine>
+              </div>
+              <div className="col-6 text-center">
+                <FirstLine>Shading Coefficient</FirstLine>
+                <SecondLine>0.9</SecondLine>
+              </div>
+            </div>
+          </ComparisonTable>
+
+          <PopupDescription>Decreasing the U-Value of the windows in turn reduces heating and cooling
+            loads.</PopupDescription>
+          <CloseBtn className="btn btn-outline-primary btn-sm mb-3" onClick={handleClose}>Close</CloseBtn>
+          </Container>
+        </Modal.Body>
+      </Modal>
+    )
   }
 
   return (
@@ -81,7 +230,12 @@ const FacilityEnvelopeElementsComparison = () => {
         <ListHeader className="col-4">Potential<br/>Best-in-class</ListHeader>
       </HeadRow>
 
-      <ItemRow className="row">
+      <ItemRow className="row"
+               onClick={() => openPopup({
+                 type: 'roof',
+                 current: facilityEnvelopeComparisonData.roof.current,
+                 potentialBestInClass: facilityEnvelopeComparisonData.roof.potentialBestInClass
+               })}>
         <ListItem className="col-4">
           <ItemImg src={roofImg} alt="Roof"/>
           <ItemImgSubTitle>Roof</ItemImgSubTitle>
@@ -93,7 +247,12 @@ const FacilityEnvelopeElementsComparison = () => {
           text={facilityEnvelopeComparisonData.roof.potentialBestInClass}/></ListItem>
       </ItemRow>
 
-      <ItemRow className="row">
+      <ItemRow className="row"
+               onClick={() => openPopup({
+                 type: 'wall',
+                 current: facilityEnvelopeComparisonData.wall.current,
+                 potentialBestInClass: facilityEnvelopeComparisonData.wall.potentialBestInClass
+               })}>
         <ListItem className="col-4">
           <ItemImg src={wallImg} alt="Wall"/>
           <ItemImgSubTitle>Wall</ItemImgSubTitle>
@@ -105,20 +264,34 @@ const FacilityEnvelopeElementsComparison = () => {
           text={facilityEnvelopeComparisonData.wall.potentialBestInClass}/></ListItem>
       </ItemRow>
 
-      <ItemRow className="row">
+      <ItemRow className="row"
+               onClick={() => openPopup({
+                 type: 'openings',
+                 current: facilityEnvelopeComparisonData.openings.current,
+                 potentialBestInClass: facilityEnvelopeComparisonData.openings.potentialBestInClass
+               })}>
         <ListItem className="col-4">
           <ItemImg src={openingsImg} alt="openings"/>
           <ItemImgSubTitle>Openings</ItemImgSubTitle>
         </ListItem>
-        <ListItem className="col-4"><EnergySquare
-          color={getCurrentColor(facilityEnvelopeComparisonData.openings.current)}
-          text={facilityEnvelopeComparisonData.openings.current}/></ListItem>
-        <ListItem className="col-4"><EnergySquare
-          color={getCurrentColor(facilityEnvelopeComparisonData.openings.potentialBestInClass)}
-          text={facilityEnvelopeComparisonData.openings.potentialBestInClass}/></ListItem>
+        <ListItem className="col-4">
+          <EnergySquare
+            color={getCurrentColor(facilityEnvelopeComparisonData.openings.current)}
+            text={facilityEnvelopeComparisonData.openings.current}/>
+        </ListItem>
+        <ListItem className="col-4">
+          <EnergySquare
+            color={getCurrentColor(facilityEnvelopeComparisonData.openings.potentialBestInClass)}
+            text={facilityEnvelopeComparisonData.openings.potentialBestInClass}/>
+        </ListItem>
       </ItemRow>
 
-      <ItemRow className="row">
+      <ItemRow className="row"
+               onClick={() => openPopup({
+                 type: 'floor',
+                 current: facilityEnvelopeComparisonData.floor.current,
+                 potentialBestInClass: facilityEnvelopeComparisonData.floor.potentialBestInClass
+               })}>
         <ListItem className="col-4">
           <ItemImg src={floorImg} alt="floor"/>
           <ItemImgSubTitle>floor</ItemImgSubTitle>
@@ -129,6 +302,9 @@ const FacilityEnvelopeElementsComparison = () => {
           color={getCurrentColor(facilityEnvelopeComparisonData.floor.potentialBestInClass)}
           text={facilityEnvelopeComparisonData.floor.potentialBestInClass}/></ListItem>
       </ItemRow>
+
+      <Popup data={popUpProps}/>
+
     </FacilityEnvelopeElementsComparisonWrapper>
   )
 }
--- src/pages/building/comparision/components/PerformanceComparison2.js
+++ src/pages/building/comparision/components/PerformanceComparison2.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import styled from 'styled-components'
+import { ResponsiveLine } from '@nivo/line'
 
-import { Line } from '@nivo/line'
-
+const ChartHeader = styled.div`
+  margin-left: 20px;
+  margin-right: 20px;
+`
 
 const PerformanceComparisonTitle = styled.h3`
   font-size: 1.15rem;
   font-weight: 700;
+  margin-bottom: 0;
+`
+
+const EditConfigurationButton = styled.button`
+  border-radius: 15px;
+  padding-left: 20px;
+  padding-right: 20px;
+  font-size: 0.8rem;
 `
 
 const PerformanceComparisonWrapper = styled.div`
@@ -15,32 +26,125 @@ const PerformanceComparisonWrapper = styled.div`
   padding: 20px;
   margin-top: 40px;
   margin-bottom: 40px;
+
+  height: 500px;
+  min-width: 100%;
+
 `
 
 const data = [{
   id: 'Design Excellent Center',
-  data:
-    [
-      {
-        'x': 'Cooling',
-        'y': '5',
+  data: [
+    {
+      'x': 'Cooling',
+      'y': '5',
 
+    },
+    {
+      'x': 'Heating',
+      'y': '5'
+    },
+    {
+      'x': 'Lighting',
+      'y': '3'
+    },
+    {
+      'x': 'Mechanical Ventilation',
+      'y': '4'
+    },
+    {
+      'x': 'Roof',
+      'y': '5'
+    },
+    {
+      'x': 'Wall',
+      'y': '4'
+    },
+    {
+      'x': 'Openings',
+      'y': '5'
+    },
+    {
+      'x': 'Floor',
+      'y': '3'
+    },
+    {
+      'x': 'Renewable',
+      'y': '3'
+    },
+    {
+      'x': 'Plug Loads',
+      'y': '3'
+    },
+  ]
+},
+  {
+    id: 'Hill Bay Central Bank Center',
+    data: [{
+      'x': 'Cooling',
+      'y': '6',
+
+    },
+      {
+        'x': 'Heating',
+        'y': '6'
       },
+      {
+        'x': 'Lighting',
+        'y': '5'
+      },
+      {
+        'x': 'Mechanical Ventilation',
+        'y': '5'
+      },
+      {
+        'x': 'Roof',
+        'y': '3'
+      },
+      {
+        'x': 'Wall',
+        'y': '3'
+      },
+      {
+        'x': 'Openings',
+        'y': '4'
+      },
+      {
+        'x': 'Floor',
+        'y': '5'
+      },
+      {
+        'x': 'Renewable',
+        'y': '4'
+      },
+      {
+        'x': 'Plug Loads',
+        'y': '2'
+      },
+    ]
+  },
+  {
+    id: 'F+E Campus',
+    data: [{
+      'x': 'Cooling',
+      'y': '4',
+
+    },
       {
         'x': 'Heating',
         'y': '5'
       },
       {
         'x': 'Lighting',
-        'y': '3'
-      },
-      {
-        'x': 'Mechanical Ventilation',
         'y': '4'
       },
       {
-        'x': 'Roof',
+        'x': 'Mechanical Ventilation',
         'y': '5'
+      },
+      {
+        'x': 'Roof',
+        'y': '4'
       },
       {
         'x': 'Wall',
@@ -56,180 +160,100 @@ const data = [{
       },
       {
         'x': 'Renewable',
-        'y': '3'
+        'y': '6'
       },
       {
         'x': 'Plug Loads',
-        'y': '3'
+        'y': '6'
       },
     ]
-},
-  {
-    id: 'Hill Bay Central Bank Center',
-    data:
-      [
-        {
-          'x': 'Cooling',
-          'y': '6',
-
-        },
-        {
-          'x': 'Heating',
-          'y': '6'
-        },
-        {
-          'x': 'Lighting',
-          'y': '5'
-        },
-        {
-          'x': 'Mechanical Ventilation',
-          'y': '5'
-        },
-        {
-          'x': 'Roof',
-          'y': '3'
-        },
-        {
-          'x': 'Wall',
-          'y': '3'
-        },
-        {
-          'x': 'Openings',
-          'y': '4'
-        },
-        {
-          'x': 'Floor',
-          'y': '5'
-        },
-        {
-          'x': 'Renewable',
-          'y': '4'
-        },
-        {
-          'x': 'Plug Loads',
-          'y': '2'
-        },
-      ]
-  },
-  {
-    id: 'F+E Campus',
-    data:
-      [
-        {
-          'x': 'Cooling',
-          'y': '4',
-
-        },
-        {
-          'x': 'Heating',
-          'y': '5'
-        },
-        {
-          'x': 'Lighting',
-          'y': '4'
-        },
-        {
-          'x': 'Mechanical Ventilation',
-          'y': '5'
-        },
-        {
-          'x': 'Roof',
-          'y': '4'
-        },
-        {
-          'x': 'Wall',
-          'y': '4'
-        },
-        {
-          'x': 'Openings',
-          'y': '5'
-        },
-        {
-          'x': 'Floor',
-          'y': '3'
-        },
-        {
-          'x': 'Renewable',
-          'y': '6'
-        },
-        {
-          'x': 'Plug Loads',
-          'y': '6'
-        },
-      ]
   }
 ]
 const commonProperties = {
-  width: 1200,
-  height: 400,
-  margin: { top: 20, right: 0, bottom: 20, left: 40 },
+  margin: { top: 0, right: 30, bottom: 0, left: 40 },
   data,
   animate: true,
-
+  height: 350
   //enableSlices: 'x',
 }
 
-
-
 const PerformanceComparison2 = () => {
 
+  const onClick = (e) => {
+    console.log(e)
+
+  }
+
   return <PerformanceComparisonWrapper>
-    <PerformanceComparisonTitle>Sub-System Performance</PerformanceComparisonTitle>
+    <ChartHeader className="d-flex justify-content-between mb-5">
+      <PerformanceComparisonTitle>Sub - System Performance</PerformanceComparisonTitle>
 
-    <Line
-      {...commonProperties}
-      curve="monotoneX"
-      data={data}
-      useMesh={false}
-      enableSlices={false}
-      enablePoint={true}
-      pointSize={12}
-      pointColor={'#fff'}
-      pointBorderWidth={1}
-      enableGridX={false}
-      lineWidth={1}
-      yScale={{
-        type: 'linear',
-        min: 1,
-        max: 7
-      }}
-      axisLeft={{
-        tickValues: [1, 2, 3, 4, 5, 6, 7],
-        format: value => {
-          const labels = ['G', 'F', 'E', 'D', 'C', 'B', 'A',]
-          return labels[value - 1]
-        }
-      }}
-      pointBorderColor={{ from: 'serieColor' }}
-      xScale={{ type: 'point' }}
-      colors={['#87972F', '#636c2e', '#c1cf74']}
-      legends={[
-        {
-          anchor: 'top',
-          direction: 'row',
-          justify: false,
-          translateX: 0,
-          translateY: -25,
-          itemWidth: 200,
-          itemHeight: 20,
-          itemsSpacing: 4,
-          symbolSize: 20,
-          symbolShape: 'circle',
-          itemDirection: 'left-to-right',
-          itemTextColor: '#777',
-          effects: [
-            {
-              on: 'hover',
-              style: {
-                itemBackground: 'rgba(0, 0, 0, .03)',
-                itemOpacity: 1
-              }
-            }
-          ]
-        }
-      ]}
+      <EditConfigurationButton
+        type="button"
+        onClick={onClick}
+        className="btn btn-primary btn-sm">Edit Comparison
+      </EditConfigurationButton>
+     
+
+    </ChartHeader>
+
+    <ResponsiveLine {...commonProperties}
+                    curve="monotoneX"
+                    data={data}
+                    useMesh={false}
+                    enableSlices={false}
+                    enablePoint={true}
+                    pointSize={12}
+                    pointColor={'#fff'}
+                    pointBorderWidth={1}
+                    enableGridX={false}
+                    lineWidth={1}
+                    yScale={
+                      {
+                        type: 'linear',
+                        min: 1,
+                        max: 7
+                      }
+                    }
+                    axisLeft={
+                      {
+                        tickValues: [1, 2, 3, 4, 5, 6, 7],
+                        format: value => {
+                          const labels = ['G', 'F', 'E', 'D', 'C', 'B', 'A',]
+                          return labels[value - 1]
+                        }
+                      }
+                    }
+                    pointBorderColor={
+                      { from: 'serieColor' }}
+                    xScale={
+                      { type: 'point' }}
+                    colors={
+                      ['#87972F', '#636c2e', '#c1cf74']}
+                    legends={
+                      [{
+                        anchor: 'top',
+                        direction: 'row',
+                        justify: false,
+                        translateX: 0,
+                        translateY: -35,
+                        itemWidth: 200,
+                        itemHeight: 20,
+                        itemsSpacing: 4,
+                        symbolSize: 20,
+                        symbolShape: 'circle',
+                        itemDirection: 'left-to-right',
+                        itemTextColor: '#777',
+                        effects: [{
+                          on: 'hover',
+                          style: {
+                            itemBackground: 'rgba(0, 0, 0, .03)',
+                            itemOpacity: 1
+                          }
+                        }]
+                      }]
+                    }
     />
-
   </PerformanceComparisonWrapper>
 }
 
