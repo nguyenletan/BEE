76835b49c0ce150188a5a344932d2fdedd5960b0
nguyenletan <nguyenletan@gmail.com>
2021-06-24 14:46:05 +1000

    added envelope and facade page

---
+++ src/reference-tables/ExternalWindowType.js
@@ -0,0 +1,20 @@
+const ExternalWallType = [
+  {
+    id: 0,
+    name: 'Curtain Wall',
+  },
+  {
+    id: 1,
+    name: 'Reinforced Concrete Wall',
+  },
+  {
+    id: 2,
+    name: 'Precast Concrete Wall',
+  },
+  {
+    id: 3,
+    name: 'Masonry Brick Wall',
+  },
+]
+
+export default ExternalWallType
