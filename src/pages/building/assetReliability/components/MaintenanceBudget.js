d122a3c8acc68436717c8aac1f610ead9c06acd0
Tan Nguyen Le <nguyenletan@outlook.com>
2021-11-16 18:11:11 +1100

    added German version

--- src/pages/building/assetReliability/components/MaintenanceBudget.js
+++ src/pages/building/assetReliability/components/MaintenanceBudget.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import BreakDown from '../../../../components/BreakDown'
+import { useTranslation } from 'react-i18next'
 
 const MaintenanceBudget = ({ data }) => {
+  const { t } = useTranslation('assetReliability')
   return (
     <BreakDown
-      title='Maintenance Budget'
+      title={t('Maintenance Budget')}
       startAngle={-0}
       data={data}
-      innerRadius={0.88}
+      innerRadius={0.58}
       isCenteredPercentage
       marginRight='0px'
       hasDescription
+      hasArcLabels={false}
     />
   )
 }
