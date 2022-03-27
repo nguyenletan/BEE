50aa698cd73eafff19eb57c33cb3e0a42e104eb0
Tan Nguyen Le <nguyenletan@outlook.com>
2022-03-27 12:52:42 +1100

    Fixed global css

--- src/components/BuildingBlockStyle.js
+++ src/components/BuildingBlockStyle.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 export const BuildingImage = styled.img`
   width: 270px;
   height: 150px;
-  margin-right: 0px;
-  @media(min-width: 768px) {
+  margin-right: 0;
+  @media (min-width: 768px) {
     margin-right: 50px;
   }
   object-fit: cover;
@@ -13,12 +13,16 @@ export const BuildingImage = styled.img`
 `
 
 export const Title = styled.h3`
-  line-height: 1.2rem;
+  line-height: 1.5;
   margin-top: .5rem;
+  margin-bottom: 0.2rem;
   font-weight: 700;
   font-size: 1.2rem;
+  color: var(--bs-primary)
 `
 
 export const Address = styled.p`
   width: 280px;
+  color: var(--bs-primary);
+  line-height: 1.5;
 `
