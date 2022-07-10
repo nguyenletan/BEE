b4c3ea1f43d00cdaa3d1d8374b5b0e0ace337791
Tan Nguyen Le <nguyenletan@outlook.com>
2022-07-10 16:30:48 +1000

    added iframe components

--- src/pages/building/comparision/components/PerformanceComparison2.js
+++ src/pages/building/comparision/components/PerformanceComparison2.js
@@ -523,7 +523,7 @@ const PerformanceComparison2 = () => {
         <PerformanceComparisonTitle className="mb-2 mb-md-0">{t(
           'Building Energy - Sub-System Performance')}</PerformanceComparisonTitle>
         <div className="d-flex">
-          <UrlButton url="comparison-building-subsystem-performance" textWidth='68ch'/>
+          <UrlButton url="comparison-building-subsystem-performance" textWidth='70ch'/>
           <EditConfigurationButton
             type="button"
             onClick={onClick}
